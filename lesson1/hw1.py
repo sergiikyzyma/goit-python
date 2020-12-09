4bb3e160ed0c1afd724241fb84ca49e2cc423b53
sergiikyzyma <teosoph@hotmail.com>
2020-12-09 15:21:46 +0200

    my first python repository

---
+++ lesson1/hw1.py
@@ -0,0 +1,9 @@
+name = input("Enter your name")
+print(name)
+x = 70
+y = 134
+result = f"{x} + {y} = {x + y}"
+print("result = ", result)
+number = input("Enter any number")
+qube = float(number) ** (1/3)
+print(f"cube root from {number} is {qube}")
