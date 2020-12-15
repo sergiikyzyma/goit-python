d582fc5891810c77458ff3c1c5a2ee6f77e1516e
sergiikyzyma <teosoph@hotmail.com>
2020-12-15 18:09:46 +0200

    my first python repositor after editing

--- lesson1/hw2.py
+++ lesson1/hw2.py
@@ -2,6 +2,6 @@ a = float(input("a "))
 b = float(input("b "))
 c = float(input("c "))
 D = b ** 2 - 4 * a * c
-x1 = ((-1) * b + D ** 0.5) / 2 * a
-x2 = ((-1) * b - D ** 0.5) / 2 * a
+x1 = ((-1) * b + D ** 0.5) / (2 * a)
+x2 = ((-1) * b - D ** 0.5) / (2 * a)
 print(f"x1 = {x1} x2 = {x2}")
