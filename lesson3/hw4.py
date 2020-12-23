a888ebd4cdd348416462ae8b7b2b5f0179f33667
sergiikyzyma <teosoph@hotmail.com>
2020-12-23 15:14:19 +0200

    completing lesson3

--- lesson3/hw4.py
+++ lesson3/hw4.py
@@ -12,9 +12,9 @@ cortImage = ("jpeg", "png", "jpg", "")
 cortVideo = ("avi", "mp4", "mov", "")
 cortText = ("doc", "dcox", "txt", "odt")
 cortMusic = ("mp3", "ogg", "wav", "amr")
-cortVarious = ("max", "vsd", "", "")
+cortVarious = ("max", "vsd", "blend", "")
 
-dictFiles = {cortFolder: "", cortImage: "", cortVideo: "", cortText: "", cortMusic: "", cortVarious: ""}
+dictFiles = {cortFolder: [], cortImage: [], cortVideo: [], cortText: [], cortMusic: [], cortVarious: []}
 
 for elem in files:
     if "." in elem:
@@ -23,17 +23,50 @@ for elem in files:
             j += 1
         else:
             if elem.endswith(cortImage[j]):
-                dictFiles[cortImage] = elem
+                dictFiles[cortImage].append(elem)
             elif elem.endswith(cortVideo[j]):
-                dictFiles[cortVideo] = elem
+                dictFiles[cortVideo].append(elem)
             elif elem.endswith(cortText[j]):
-                dictFiles[cortText] = elem
+                dictFiles[cortText].append(elem)
             elif elem.endswith(cortMusic[j]):
-                dictFiles[cortMusic] = elem
+                dictFiles[cortMusic].append(elem)
             elif elem.endswith(cortVarious[j]):
-                dictFiles[cortVarious] = elem
+                dictFiles[cortVarious].append(elem)
     else:
-        dictFiles[cortFolder] = elem
+        dictFiles[cortFolder].append(elem)
 
-    for key, value in dictFiles.items():
-        print(f"Keys = {key} Values = {value}")
+while True:
+    os.system("clear")
+    find = input("Enter that you wanna find (folder, image, video, text, various or all) or exit\t")
+    if find == "folder":
+        key = cortFolder
+    elif find == "image":
+        key = cortImage
+    elif find == "video":
+        key = cortVideo
+    elif find == "text":
+        key = cortText
+    elif find == "various":
+        key = cortVarious
+    elif find == "all":
+        for key, value in dictFiles.items():
+            if key == cortFolder:
+                print(f"\nList of folders ({key}) : {value}")
+            elif key == cortImage:
+                print(f"\nList of images ({key}) : {value}")
+            elif key == cortVideo:
+                print(f"\nList of video ({key}) : {value}")
+            elif key == cortText:
+                print(f"\nList of documents ({key}) : {value}")
+            elif key == cortMusic:
+                print(f"\nList of musics ({key}) : {value}")
+            elif key == cortVarious:
+                print(f"\nList of various files ({key}) : {value}")
+        input("\nPress any key to continue")
+        break
+    elif find == "exit":
+        input("\nPress any key to continue")
+        break
+    for value in dictFiles.get(key):
+        print(value)
+    input("\nPress any key to continue")
