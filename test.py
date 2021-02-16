2d71ce8ab0bca03a8d4c858db292e4f9e747fd24
sergiikyzyma <teosoph@hotmail.com>
2021-02-16 12:21:09 +0200

    virtual_environment

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-'''
+import sys
+
+"""
 import string
 surname = 'Stephen Spielberg'
 letters = (set(surname) & set(string.ascii_uppercase)) | ((set(surname) & set(string.ascii_lowercase)) ^ set('abcde'))
@@ -118,7 +120,7 @@ def custom_map(func, *seqs):
     return result
 
 print("result = ", custom_map(func, 2, 3, 5, (-3, 34)))
-'''
+
 NAME = "Business District"
 URL = "Website"
 with open("2018_Seattle_Business_Districts.csv", "r") as fm:
@@ -138,3 +140,151 @@ with open("2018_Seattle_Business_Districts.csv", "r") as fm:
             print("\n", name_data, " ", url_data, "\n")
             html.write(f"<li><a href='{url_data}'>{name_data}</a></li>")
         html.write("</ul></body></html>")
+
+print(sys.path)
+
+surname_to_search = "Spielberg"
+surnames_statistic_list = ["Stephen_Spielberg_salary_statistic.xlsx", "Jeff_Mikelson_salary_year.xlsx", "Bosh_Ray_salary.xlsx"]
+surnames_income = {"Mikelson": 25000, "Bosh": 16000}
+taxes_pay = {"Mikelson", "Bosh"}
+not_proper_declaration = []
+statistic_flag = False
+for statistic in surnames_statistic_list:
+    if statistic.find(surname_to_search):
+        statistic_flag = True
+
+if statistic_flag:
+    not_proper_declaration.append("statistic_list")
+if surname_to_search in surnames_income.keys():
+    not_proper_declaration.append("income")
+if surname_to_search in taxes_pay:
+    not_proper_declaration.append("taxes")
+
+print(not_proper_declaration)
+
+price = 80
+discount_percent = 20
+def discount(price, discount_percent):
+    price *= discount_percent / 100
+    return price
+final_price = price - discount(price, discount_percent)
+print(price, final_price)
+
+def add_premium(salary_dict, premium_percent = 20):
+    for name, salary in salary_dict.items():
+        salary *= (100 + premium_percent) / 100
+        salary_dict[name] = salary
+    return salary_dict
+print(add_premium({"Spielberg": 8000, "Bosh": 1200, "Khamraev": 11000}))
+
+def is_prime(n, d=3):
+    if n <= d: return n > 1    
+    elif not n % 2 or not n % 3: return False   
+    else:
+        j = 5
+        for i in range(j, j ** 2 <= n, 6):
+            if not n % i or not n % (i + 2): return False   
+        return True
+print(" 7 ", is_prime(7))
+
+import math
+
+def shift_variations_count(workers_list, shift_count):
+    n = 10
+    for _ in workers_list:
+        n += 1
+    print(workers_list," ", n, " ", shift_count)
+    return math.factorial(n) / (math.factorial(shift_count) * math.factorial(n - shift_count))
+print(shift_variations_count(surnames_statistic_list, 3))
+"""
+lines = ["Central 6000", "Centra 600", "Centr 60", "tral 12000", "Ceral 16000"]
+summa = 0
+for _ in lines:
+    line = _.split()
+    summa += float(line[1])
+print(summa)
+"""
+orders = ["Big chicken:active", "Big:active", "chicken:active"]        
+fh = open("text.txt", "a")
+for order in orders:
+    fh.writelines(order + "\n")
+fh.close()
+fd = open("text.txt", "r")
+lines = fd.readlines()
+count = 0
+for _ in lines:
+    line = _.split(":")
+    if line[1][:-1] == "active":        
+        count += 1
+fd.close()        
+print(count)
+
+def is_equal(utf_8_pass, utf_16_pass):
+    if utf_8_pass.decode("utf-8") == utf_16_pass.decode("utf-16"):
+        return True
+    else:
+        return False
+print(is_equal(lines[0].encode("utf-8"), lines[0].encode("utf-16")))
+
+import shutil
+def create_archive(path, file_name, employee_citizenship):
+    with open(path + "/" + file_name, "ab") as fh:
+        for name, citizenship in employee_citizenship.items():
+            temp = str(name) + citizenship + "\n"
+            fh.write(temp.encode())
+    shutil.make_archive("backup", "zip", path)
+
+    return file_name
+my_dict = dict()
+for i, my_str in enumerate(lines):
+    my_dict[i] = my_str
+create_archive(shutil.os.getcwd(), "text.bin", my_dict)
+
+def comments_to_show(comment_dict={}):
+    comment_list = []
+    new_comment_list = []
+    for comment in comment_dict.items():
+        comment_list.append(comment)
+    comment_list.sort(key=lambda x: x[1], reverse=True)
+    for new_comment in comment_list[:3]:
+        new_new_comment = new_comment[0].removeprefix("<comment>").removesuffix("</comment>").strip()
+        new_comment_list.append(new_new_comment)
+    comment_result = "\n".join(new_comment_list)
+    result = comment_result.replace("\t", "")
+    return result
+
+print(comments_to_show({"vdgfg": 23, "cnjdn": 34, "av": 3, "23": 7, "nmnm": 9}))
+"""
+
+def translate_surnames(surnames):
+    alphabet_rus = ("а", "б", "е", "и", "к", "о", "р", "н", "т", "с", "л")
+    alphabet = ("a", "b", "e", "i", "k", "o", "r", "n", "t", "s", "l")
+    map = dict()
+    for i, _ in enumerate(alphabet_rus):
+        map[ord(alphabet_rus[i])] = alphabet[i]
+        map[ord(alphabet_rus[i].upper())] = alphabet[i].capitalize()
+    name = surnames.translate(map)
+    return name
+print(translate_surnames("Сергей"))
+
+def formatted_comments(comments_dict):
+    new_comment = []
+    for comment in comments_dict.items():
+        new_comment.append("|{:^20}|{:>20}|".format(comment[0], comment[1]))
+        print("|{:^20}|{:>20}|".format(comment[0], comment[1]))
+    return new_comment
+formatted_comments({"Nice service": 100, "Polite waitresses": 39, "Delicious food": 80})
+
+import re
+def structure_recipe(order):
+    # res = re.sub(("\d"), " ", recipe)
+    # result = re.sub((";"), "\n", res)
+    res = re.findall("[:][ ]\d+", order)
+    i = 0
+    summa = 0
+    for _ in res:
+        summa += int(res[i][2:])
+        i += 1
+    print(i, " ", res)
+    return summa
+print(structure_recipe("dheud37r6: 7473t46;dheud37r6: 743t46;dheud37r6: 773t46;"))
